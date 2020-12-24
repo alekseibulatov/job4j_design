56a112b7e7a469f1cda11cd6bcb0872b2d0c9258
Aleksei Bulatov <bulatov2803@gmail.com>
2020-12-24 18:34:35 +0300

    Что такое обобщенные типы (generics) [#425398]

---
+++ src/main/java/ru/job4j/generics/Animal.java
@@ -0,0 +1,9 @@
+package ru.job4j.generics;
+
+public class Animal {
+//    private String s;
+//
+//    public Animal(String s) {
+//        this.s = s;
+//    }
+}
---
+++ src/main/java/ru/job4j/generics/Predator.java
@@ -0,0 +1,10 @@
+package ru.job4j.generics;
+
+public class Predator extends Animal {
+//    private String a;
+//
+//    public Predator(String s, String a) {
+//        super(s);
+//        this.a = a;
+//    }
+}
